c0f6b6c45d8b24225d0829bc32928dfcc294bacf
Justin Garner <charlottewebsolutions1@gmail.com>
2023-09-19 11:44:46 -0400

    messed with svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -4,13 +4,17 @@ import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: sveltePreprocess({
-		sass: {
-			renderSync: true,
-		},
-	}),
+	kit: {
+		adapter: adapter()
+	},
+	preprocess: [
+		sveltePreprocess({
+			sass: {
+				renderSync: true,
+			},
+		}),
+		vitePreprocess()
+	]
 };
 
 export default config;
