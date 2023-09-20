479de79fc5536b2cade7c6d4188185fd85103220
Justin Garner <charlottewebsolutions1@gmail.com>
2023-09-20 14:52:44 -0400

    wow just completed v1 of the instagram api, will increase speed, chance of working, clean up and other improvements soon but right now you can get the most recent 4 messages as well as send messages to a specific user, incredible. It surreal to see it working!

--- api/index.js
+++ api/index.js
@@ -26,22 +26,6 @@ async function main() {
         console.log('Navigated! Scraping page content...');
         await page.waitForTimeout(5000);
         await page.screenshot({ path: 'screenshot1.png' });
-
-
-    await page.screenshot({ path: 'screenshot2.png' });
-
-            setTimeout(async () => {
-                await page.screenshot({ path: 'screenshot3.png' });
-                await page.type('#loginForm > div > div:nth-child(1) > div > label > input', username, {delay: 50})
-                
-                await page.type('#loginForm > div > div:nth-child(2) > div > label > input', password, {delay: 50})
-                await page.screenshot({ path: 'screenshot4.png' });
-                
-                    console.log(`Logging in`)
-                    await page.click('#loginForm > div > div:nth-child(3) > button')
-                    await page.waitForTimeout(5000)
-                    await page.screenshot({ path: 'screenshot5.png' });
-            }, 5000);
     } 
 
         //const searchResultSelector = 'body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe.x5yr21d.x19onx9a > div > button._a9--._a9_0';
---
+++ api/test2.js
@@ -0,0 +1,175 @@
+import puppeteer from 'puppeteer-extra';
+import delay from 'delay';
+import RecaptchaPlugin from 'puppeteer-extra-plugin-recaptcha';
+//import SmartProxyPlugin from 'zyte-smartproxy-plugin';
+
+//puppeteer.use(SmartProxyPlugin({
+ // spm_apikey: 'f49b002e803b4fa78261293b537be925',
+//}));
+
+puppeteer.use(
+  RecaptchaPlugin({
+    provider: {
+      id: '2captcha',
+      token: 'da621fac37b3615cae4b5cec2b187f6c' // REPLACE THIS WITH YOUR OWN 2CAPTCHA API KEY ⚡
+    },
+    visualFeedback: true // colorize reCAPTCHAs (violet = detected, green = solved)
+  })
+)
+
+const user = {
+    username: 'justingarnerphotography',
+    password: 'Sunglasses19',
+};
+
+puppeteer.launch(
+    {
+    headless: false,
+    args: [
+    '--disable-features=IsolateOrigins,site-per-process,SitePerProcess',
+    '--flag-switches-begin --disable-site-isolation-trials --flag-switches-end'
+  ]
+}).then(async browser => {
+    sendMessage(browser, user, {}, 'yo mama', 'justinfilms_');
+});
+
+let getRecentMessages = async (browser, user, opts) => {
+    const page = await browser.newPage(); //{ignoreHTTPSErrors: true}
+    await page.goto('https://www.instagram.com/', {timeout: 180000});
+
+    await page.waitForSelector('input[name=username]', { visible: true });
+    await delay(100);
+    await page.type('input[name=username]', user.email || user.username, { delay: 50 });
+
+    await delay(100);
+    await page.type('input[name=password]', user.password, { delay: 50 });
+
+    await delay(100);
+    const [ signin ] = await page.$x('//button[contains(.,"Log in")]');
+    await signin.click({ delay: 30 });
+
+    await page.waitForTimeout(5000);
+
+    await delay(5000);
+    console.log('about to look for captcha');
+    // Loop over all potential frames on that page
+    for (const frame of page.mainFrame().childFrames()) {
+        // Attempt to solve any potential captchas in those frames
+        const { solved, error } = await frame.solveRecaptchas();
+        if(solved){
+            delay(1000);
+            await page.waitForSelector('.x1i10hfl');  // Wait for the button to be visible
+            await page.click('.x1i10hfl', { delay: 30 });
+        }
+    }
+
+    await page.waitForTimeout(5000);
+
+    await page.goto('https://www.instagram.com/direct/inbox/');
+
+    await page.waitForTimeout(5000);
+
+    const popupExists = await page.$('div._a9-v');
+    if (popupExists) {
+        await page.click('button._a9--._a9_1');
+        await page.waitForTimeout(5000);  // Click the button
+    }
+
+    const chats = await page.$$('img.x5yr21d');
+    console.log(chats, 'chats');
+
+    await chats[1].click({ delay: 30 });
+
+    await page.waitForTimeout(5000);
+
+    const chatWindow = await page.$('.x6prxxf');
+    const boundingBox = await chatWindow.boundingBox();
+    await page.mouse.move(
+        boundingBox.x + boundingBox.width / 2,
+        boundingBox.y + boundingBox.height * 4
+    );
+
+    await page.mouse.wheel({deltaY: 800});
+
+    await page.evaluate(() => {
+        const chatWindow = document.querySelector('.x6prxxf');
+        chatWindow.scrollTop -= 800;  // Adjust the value as needed for the desired scroll amount
+      });
+
+      const messages = await page.$$eval('div.x1tlxs6b div.xzsf02u, div.x1tlxs6b div.x14ctfv', (elements) => {
+        return elements.map((element) => {
+          const sender = element.classList.contains('x14ctfv') ? 'you' : 'other person';
+          const messageText = element.textContent;
+          return `${sender}: ${messageText}`;
+        });
+      });
+      
+      console.log(messages);
+      
+
+
+
+};
+
+let sendMessage = async (browser, user, opts, message, recipient) => {
+  const page = await browser.newPage(); //{ignoreHTTPSErrors: true}
+  await page.goto('https://www.instagram.com/', {timeout: 180000});
+
+  await page.waitForSelector('input[name=username]', { visible: true });
+  await delay(100);
+  await page.type('input[name=username]', user.email || user.username, { delay: 50 });
+
+  await delay(100);
+  await page.type('input[name=password]', user.password, { delay: 50 });
+
+  await delay(100);
+  const [ signin ] = await page.$x('//button[contains(.,"Log in")]');
+  await signin.click({ delay: 30 });
+
+  await page.waitForTimeout(5000);
+
+  await page.goto(`https://www.instagram.com/${recipient}/`);
+
+  await page.waitForTimeout(5000);
+
+  const btns = await page.$$('[role=button]');
+  for(let btn of btns){
+    const text = await btn.evaluate(el => el.textContent);
+    if(text === 'Message'){
+      await btn.click({ delay: 30 });
+      break;
+    }
+  }
+
+
+  await page.waitForTimeout(5000);
+
+  const popupExists = await page.$('div._a9-v');
+  if (popupExists) {
+      await page.click('button._a9--._a9_1');
+      await page.waitForTimeout(5000);  // Click the button
+  }
+
+  await page.waitForTimeout(5000);
+
+  const input = await page.$('.x1qjc9v5 .xzsf02u');
+
+  await input.type(message, { delay: 100 });
+
+  await page.click('div.x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.xdl72j9.x2lah0s.xe8uvvx.xdj266r.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1q0g3np.x1lku1pv.x1a2a7pz.x6s0dn4.xjyslct.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x9f619.x1ypdohk.x1i0vuye.x1f6kntn.xwhw2v2.xl56j7k.x17ydfre.x2b8uid.xlyipyv.x87ps6o.x14atkfc.xcdnw81.xjbqb8w.xm3z3ea.x1x8b98j.x131883w.x16mih1h.x972fbf.xcfux6l.x1qhh985.xm0m39n.xt0psk2.xt7dq6l.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x1n5bzlp.x173jzuc.x1yc6y37.xfs2ol5');
+};
+
+
+/* await delay(5000);
+  console.log('about to look for captcha');
+  // Loop over all potential frames on that page
+  for (const frame of page.mainFrame().childFrames()) {
+      // Attempt to solve any potential captchas in those frames
+      const { solved, error } = await frame.solveRecaptchas();
+      if(solved){
+          delay(1000);
+          await page.waitForSelector('.x1i10hfl');  // Wait for the button to be visible
+          await page.click('.x1i10hfl', { delay: 30 });
+      }
+  }
+*/
